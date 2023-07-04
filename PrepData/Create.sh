14774e23c7831146cfa9a7133cc510647ba5e36c
cweng <cweng@c3b15.wi.mit.edu>
2023-07-04 16:01:18 -0400

    new file:   PrepData/Create.sh 	new file:   PrepData/prep.py

---
+++ PrepData/Create.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+sample=$1
+mkdir $sample
+mkdir $sample/FASTQ
+mkdir $sample/FASTQ/Mito
+mkdir $sample/FASTQ/ATAC
+mkdir $sample/FASTQ/RNA
+mkdir $sample/CellRanger
+mkdir $sample/Mito
+mkdir $sample/Mito/Enrich
+mkdir $sample/Mito/WholeATAC
+# mkdir FASTQ/Hash
+# mkdir HashResolve
